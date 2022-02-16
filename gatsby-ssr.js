abd1268dc0432060705ef4622b9cc8c53653f02c
jakubchrapek <chrapekuba@gmail.com>
2022-02-16 20:02:10 +0100

    Gatsby ssr imports

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,15 @@
-const React = require('react')
-const Layout =
-  require('./src/components/commonLayout').default
+import React from 'react'
+import Layout from './src/components/commonLayout'
+import '@fontsource/k2d/300.css'
+import '@fontsource/k2d/400.css'
+import '@fontsource/k2d/500.css'
+import '@fontsource/k2d/600.css'
+import '@fontsource/titillium-web/400.css'
+import '@fontsource/titillium-web/700.css'
+import { BlockBodyProvider } from './src/components/blockBodyContext'
 
-exports.wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  return (
-    <BlockBodyProvider>
-      <Layout {...props}>{element}</Layout>
-    </BlockBodyProvider>
-  )
-}
+export const wrapPageElement = ({ element, props }) => (
+  <BlockBodyProvider>
+    <Layout {...props}>{element}</Layout>
+  </BlockBodyProvider>
+)
